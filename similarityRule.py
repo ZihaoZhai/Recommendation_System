2d7368ff3f2eec9c3190ca7c3eb919d5216ca165
HAOYU-LI <haoyu@evestemptation.com>
2018-12-26 09:48:55 -0800

    update indentation

--- similarityRule.py
+++ similarityRule.py
@@ -181,46 +181,47 @@ def add_filterStyle(filterStyle, style_dict):
     return res
 
 def main():
-	sql = "select price, color, simple_color, filterbrand, filterstyle from product \
+    cur = connect(env['basicRulesParameter']['PostgreSqlConnectParameter'])
+    sql = "select price, color, simple_color, filterbrand, filterstyle from product \
        where category_path <> 'category/materials' or category_path is null"
-	cur.execute(sql)
-	row = cur.fetchone()
+    cur.execute(sql)
+    row = cur.fetchone()
 
-	priceInterval = price_range(env, prices)
-	simple_colors = fetch_simpleColors(cur)
-	color_dict = simpleColor_dict(simple_colors)
-	style_dict = fetch_filterStyle(cur)
+    priceInterval = price_range(env, prices)
+    simple_colors = fetch_simpleColors(cur)
+    color_dict = simpleColor_dict(simple_colors)
+    style_dict = fetch_filterStyle(cur)
 
-	while row:
-	    price = float(row[0])
-	    color = row[1]
-	    simple_color = row[2]
-	    brand = row[3]
-	    filterStyle = row[4]
-	    
-	    price_vector = add_price(price, priceInterval)
-	    simpleColor_vector = add_simpleColor(simple_color, color_dict)
-	    brightness_vector = add_brightness(color)
-	    brand_vector = add_filterBrands(brand)
-	    style_vector = add_filterStyle(filterStyle, style_dict)
-	    
-	    print('price_vector : ', price_vector)
-	    print('price : ', price)
-	    print('simpleColor_vector : ', simpleColor_vector)
-	    print('simple_color : ', simple_color)
-	    print('brightness_vector : ', brightness_vector)
-	    print('color : ', color)
-	    print('brand_vector : ', brand_vector)
-	    print('brand : ', brand)
-	    print('style_vector : ', style_vector)
-	    print('filterStyle : ', filterStyle)
-	    
-	    row = cur.fetchone()
+    while row:
+        price = float(row[0])
+        color = row[1]
+        simple_color = row[2]
+        brand = row[3]
+        filterStyle = row[4]
+        
+        price_vector = add_price(price, priceInterval)
+        simpleColor_vector = add_simpleColor(simple_color, color_dict)
+        brightness_vector = add_brightness(color)
+        brand_vector = add_filterBrands(brand)
+        style_vector = add_filterStyle(filterStyle, style_dict)
+        
+        print('price_vector : ', price_vector)
+        print('price : ', price)
+        print('simpleColor_vector : ', simpleColor_vector)
+        print('simple_color : ', simple_color)
+        print('brightness_vector : ', brightness_vector)
+        print('color : ', color)
+        print('brand_vector : ', brand_vector)
+        print('brand : ', brand)
+        print('style_vector : ', style_vector)
+        print('filterStyle : ', filterStyle)
+        
+        row = cur.fetchone()
 
 class Item :
-	'''
-		Item class that is used to calculate similarity among different items
-	'''
+    '''
+        Item class that is used to calculate similarity among different items
+    '''
     def __init__(self, env, price, color, simple_color, brand, filterStyle):
         self.price = price
         self.color = color
@@ -244,31 +245,31 @@ class Item :
         if env['similarityRulesParameter']['useFilterStyle']:
             style_dict = fetch_filterStyle(cur)
             self.style_vector = add_filterStyle(filterStyle, style_dict)
-    
+
     def Jacard_similarity(self, vec1, vec2):
         if vec1 is None or vec2 is None or len(vec1) != len(vec2):
             return 0
         return float(sum([i for i in range(len(vec1)) if vec1[i] == vec2[i] and vec1[1] == 1])) / \
                sum([i for i in range(len(vec1)) if vec1[i] == 1 or vec2[1] == 1])
-    
+
     def Cosint_similarity(self, vec1, vec2):
         pass
-    
+
     def calculate_similarity(self, vec1, vec2):
         pass
         
-    
+
     def price_similarity(self, item):
         return self.calculate_similarity(self.price_vector, item.price_vector)
-    
+
     def color_similarity(self, item):
         return self.calculate_similarity(self.color_vector, item.color_vector)
-    
+
     def brightness_similarity(self, item):
         return self.calculate_similarity(self.brightness_vector, item.brightness_vector)
-    
+
     def brand_similarity(self, item):
         return self.calculate_similarity(self.brand_vector, item.brand_vector)
-    
+
     def style_similarity(self, item):
         return self.calculate_similarity(self.style_vector, item.style_vector)
